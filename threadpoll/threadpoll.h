1889427530fa13205c8f61752f87216b5412fcbe
mojiez <937691466@qq.com>
2023-08-08 13:07:50 +0800

    threadpoll.h done

--- threadpoll/threadpoll.h
+++ threadpoll/threadpoll.h
@@ -13,7 +13,7 @@ class threadpoll{
     public:
         // thread_number是线程池中线程的数量
         // max_requests是请求队列中最多允许的、等待处理的请求的数量
-        threadpoll(int thread_number=8,int max_request=10000);
+        threadpoll(connection_pool *connPool,int thread_number=8,int max_request=10000);
         ~threadpoll();
 
         // 向请求队列中插入任务请求
@@ -70,4 +70,81 @@ class threadpoll{
         connection_pool* m_connPool;
 
 };
+
+template<typename T>
+threadpoll<T>::threadpoll(connection_pool *connPool,int thread_number,int max_request):m_thread_number(thread_number),m_max_requests(max_request),m_connPool(connPool),m_stop(false),m_thread(NULL){
+    if(m_thread_number<=0||m_max_requests<=0){
+        throw std::exception();
+    }
+    m_threads = new thread_t[thread_number];
+    if(!m_threads){
+        throw std::exception();
+    }
+    for(int i=0;i<thread_number;i++){
+        if(pthread_create(m_threads+i,NULL,worker,this)!=0){
+            delete[] m_threads;
+            throw std::exception();
+        }
+        if(pthread_detach(m_threads[i])>0){
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+
+}
+
+template<typename T>
+threadpoll<T>::~threadpoll(){
+    delete[] m_threads;
+    m_stop = true;
+    
+}
+
+template<typename T>
+bool threadpoll<T>::append(T* request){
+    //appen a request
+    // first lock
+    m_queuelocker.lock();
+    
+    if(m_workqueue.size()>m_max_requests){
+        m_queuelocker.unlock();
+        return false;
+    }
+
+    m_workqueue.push_back(request);
+    m_queuestat.post();
+    m_queuelocker.unlock();
+    return true;
+}
+
+template<typename T>
+void* threadpoll<T>::worker(void *arg){
+    threadpoll *poll = arg;
+    poll->run();
+    return poll;
+}
+
+template<typename T>
+void threadpoll<T>::run(){
+    // zhu xun huan
+    while(!m_stop){
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        
+        if(m_workqueue.empty()){
+            m_queuelocker.unlock();
+            continue;
+        }
+
+        T *request = m_workqueue.front();
+        m_workqueue.pop_front();
+        m_queuelocker.unlock();
+
+        if(request==NULL) continue;
+        // TODO connectionRAII
+
+        //TODO request
+        request->process();
+    }
+}
 #endif 
